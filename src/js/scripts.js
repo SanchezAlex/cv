f309f264343e981218a7dfd271689f597886199a
SanchezAlex <malinovskiy.alexandr@gmail.com>
2016-11-17 00:25:13 +0200

    finished contact section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     $('.progress .progress-bar').progressbar({display_text: 'none', use_percentage: false});
 });
+
+$(document).ready(function init_map() {
+    var myLocation = new google.maps.LatLng(50.474118, 30.515123);
+    var mapOptions = {
+        center: myLocation,
+        zoom: 13
+    };
+    var marker = new google.maps.Marker({
+        position: myLocation,
+        title: "Property Location"
+    });
+    var map = new google.maps.Map(document.getElementById("address-map"),
+        mapOptions);
+    marker.setMap(map);
+});
