afa61bcab0bc859e541410ec602b2b84fc421314
SanchezAlex <malinovskiy.alexandr@gmail.com>
2016-11-23 21:06:07 +0200

    intro-block translate3d

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,29 +3,34 @@ $(document).ready(function() {
 });
 
 $(document).ready(function initMap() {
-    var myLocation = new google.maps.LatLng(50.474118, 30.515123);
-    var mapOptions = {
-        center: myLocation,
-        zoom: 13
-    };
-    var marker = new google.maps.Marker({
-        position: myLocation,
-        title: "Property Location"
+    var b = $('.intro-banner');
+    var w = $(window);
+    var c = $('.intro-block');
+    (function () {
+        var myLocation = new google.maps.LatLng(50.474118, 30.515123);
+        var mapOptions = {
+            center: myLocation,
+            zoom: 13
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "Property Location"
+        });
+        var map = new google.maps.Map(document.getElementById("address-map"),
+            mapOptions);
+        marker.setMap(map);
+    })();
+
+
+    $(document).scroll(function(){
+        parallax();
     });
-    var map = new google.maps.Map(document.getElementById("address-map"),
-        mapOptions);
-    marker.setMap(map);
+
+    function parallax() {
+        var scrolled = w.scrollTop();
+        b.css('transform', 'translate3d(0, ' + -(scrolled * 0.1) + 'px, 0)');
+        // $('.intro-block').css('top', 20 + (scrolled * 0.1) + '%');
+        c.css('opacity', 1 - (scrolled * 0.01) / 10);
+        // $('.intro-block').css('opacity', 1 - (scrolled * 0.01) / 10);
+    }
 });
-
-$(document).scroll(function(){
-    parallax();
-});
-
-function parallax() {
-    var scrolled = $(window).scrollTop();
-    $('.intro-banner').css('top', -(scrolled * 0.1) + 'px');
-    $('.intro-block').css('top', 50 + (scrolled * 0.1) + '%');
-    $('.intro-block').css('opacity', 1 - (scrolled * 0.01) / 10);
-    $('.intro-block').css('opacity', 1 - (scrolled * 0.01) / 10);
-
-}
